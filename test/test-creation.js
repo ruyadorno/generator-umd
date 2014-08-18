fda689acd21cf047f268c007c802a22d60a4e4cd
Ruy Adorno <ruyadorno@hotmail.com>
2014-08-17 22:29:20 -0400

    Added tests on generated project as part of tests suit

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,6 +3,7 @@
 
 var path = require('path');
 var yo = require('yeoman-generator');
+var exec = require('child_process').exec;
 
 
 // ---
@@ -35,7 +36,8 @@ describe('umd generator', function () {
       'bower.json',
       'package.json',
       'README.md',
-      'somename.js'
+      'somename.js',
+      'test/somenameSpec.js'
     ];
 
     yo.test.mockPrompt(this.app, {
@@ -56,14 +58,14 @@ describe('umd generator', function () {
   it('prompted options are correctly used', function (done) {
 
     yo.test.mockPrompt(this.app, {
-      'moduleName': 'somename',
+      'moduleName': 'Super module yay',
       'description': 'bla bla bla',
       'repository': 'https://github.com/ruyadorno/generator-umd.git'
     });
 
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
-      yo.assert.fileContent('package.json', /somename/);
+      yo.assert.fileContent('package.json', /super\-module\-yay/);
       yo.assert.fileContent('package.json', /bla\ bla\ bla/);
       yo.assert.fileContent('package.json', /github\.com/);
       done();
@@ -111,6 +113,32 @@ describe('umd generator', function () {
 
       yo.assert.textEqual(myinstance.someMethod('hello '), 'hello value');
       done();
+    });
+  });
+
+
+  // ---
+
+
+  it('should be able to run tests on generated module', function (done) {
+
+    yo.test.mockPrompt(this.app, {
+      'moduleName': 'somename'
+    });
+
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+
+      exec('npm test', {}, function execDone(err, stdout, stderr) {
+
+        if (err) {
+          yo.assert.fail(1, 0, stderr);
+        }
+
+        console.log(stdout);
+
+        done();
+      });
     });
   });
 });
